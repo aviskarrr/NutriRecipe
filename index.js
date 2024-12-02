a022e861f29782d565346d6bee07235942d478ee
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2024-12-02 14:35:34 +0545

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+module.exports = app; // for testing
