958b6c698a5337c9f3ee0ba42f7a4654c3fa7e56
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2024-12-04 17:34:48 +0545

    apiLogin

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-const express = require('express');
+// const express = require('express');
 
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+// const userRoute = require('./routers/user.route');
+
+
+// const app = express();
+
+// // Replace body-parser with express.json()
+// app.use(express.json()); // Parses JSON data in the request body
+// //used for checking the request body
+
+// app.use('/', userRoute);
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({
+//         success: false,
+//         message: err.message,
+//     });
+// });
+
+
+// module.exports = app; // for testing 
+
+
+
+const express = require('express');
 const userRoute = require('./routers/user.route');
+const bodyParser = require('body-parser');
+
 
 
 const app = express();
-
-app.use(bodyParser.json()); // for parsing application/json
-//used for checking the request body
+app.use(bodyParser.json()); // Parses application/json
+// app.use(express.json()); // Parses incoming JSON requests
 
 app.use('/', userRoute);
-module.exports = app; // for testing 
+
+module.exports = app;
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,44 @@
-const userService = require('../services/user.services');
+// const userService = require('../services/user.services');
 
-exports.register = async(req, res, next)=>{
-    try{
-        const {email, password} = req.body;
+// exports.register = async(req, res, next)=>{
+//     try{
+//         console.log('Request body:', req.body);
+//         const {email, password} = req.body;
+
+//         const successRes = await userService.registerUser(email, password);
+
+//         res.json({
+//             success:true,
+//             message:'User registered successfully',
+//             data:successRes
+//         });
+
+//     }catch(error){
+//         next(error);
+//     }
+// };
+
+
+
+exports.register = async (req, res, next) => {
+    try {
+        console.log('Headers:', req.headers); // Log headers to confirm content type
+        console.log('Request body:', req.body); // Log raw request body
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            throw new Error('Email and password are required.');
+        }
 
         const successRes = await userService.registerUser(email, password);
 
         res.json({
-            success:true,
-            message:'User registered successfully',
-            data:successRes
+            success: true,
+            message: 'User registered successfully',
+            data: successRes,
         });
-    }catch(error){
+    } catch (error) {
+        console.error('Error in registration:', error);
         next(error);
     }
 };
-
